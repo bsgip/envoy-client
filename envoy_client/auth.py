1540e89085189149ebbd924a690762cb95f75415
bennyweise <ben.weise@gmail.com>
2021-08-23 09:19:06 +1000

    Add client-side certificate auth

--- envoy_client/auth.py
+++ envoy_client/auth.py
@@ -25,3 +25,25 @@ class LocalModeXTokenAuth:
         # Convert to integer as this is what is checked against
         session.headers['X-Token'] = str(int(self.lfdi, 16))
         session.headers['X-Forwarded-Client-Cert'] = ""  # Required for local auth
+
+
+class ClientCerticateAuth(Auth):
+    def __init__(self, cert: str) -> None:
+        """Authorisation based on the supplied path to a client-side certificate.
+        This certificate is normally issued by the relevant Certificate Authority 
+        that the utility server is using. For testing purposes, this may be a self-signed
+        certificate.
+
+        Args:
+            cert (str): Path to the client certificate
+        """
+        super().__init__()
+        self.cert = cert
+
+    def update_session(self, session: Session) -> None:
+        """Update transport session with relevant authorisation information
+
+        Args:
+            session (Session): Transport `Session` object
+        """
+        session.cert = self.cert
